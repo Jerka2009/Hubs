367ef6f32628a4ca11a53cca59728c1a2393d1fb
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-03-01 19:36:21 +0500

    Update OriganeHub.lua

--- OriganeHub.lua
+++ OriganeHub.lua
@@ -10,6 +10,7 @@ local PlayerSection = Player:NewSection("Player")
 local ToolsSection = Tools:NewSection("Tools")
 -- Variables
 local Players = game.Players:GetPlayers()
+local UserInputService = game:GetService("UserInputService")
 local TeleportService = game:GetService("TeleportService")
 local infjumpenabled = false
 local Noclip = nil
@@ -83,26 +84,38 @@ PlayerSection:NewButton("Reset", "RespawnCaracter", function()
 	wait()
 	game.Players.LocalPlayer.Character = char
 	newChar:Destroy()
-    print("Clicked")
+end)
+
+PlayerSection:NewButton("Godmode", "Inf Health", function()
+	local char = game.Players.LocalPlayer.Character
+	if char:FindFirstChildOfClass("Humanoid") then char:FindFirstChildOfClass("Humanoid"):ChangeState(15) end
+	char:ClearAllChildren()
+	local newChar = Instance.new("Model")
+	newChar.Parent = workspace
+	game.Players.LocalPlayer.Character = newChar
+	wait()
+	game.Players.LocalPlayer.Character = char
+	newChar:Destroy()
+    print("GodMode Enabled")
 end)
 
 PlayerSection:NewToggle("InfJump", "On / Off", function(state)
     if state then
 		infJumpOn()
-        print("Toggle On")
+        print("infJump On")
     else
 		infJumpOff()
-        print("Toggle Off")
+        print("infJump Off")
     end
 end)
 
 PlayerSection:NewToggle("Noclip", "On / Off", function(state)
     if state then
 		noclip()
-        print("Toggle On")
+        print("Noclip On")
     else
 		clip()
-        print("Toggle Off")
+        print("Noclip Off")
     end
 end)
 -- Tool Tab
@@ -110,7 +123,7 @@ ToolsSection:NewButton("TpTool", "Teleport Tool", function()
 	mouse = game.Players.LocalPlayer:GetMouse()
 	tool = Instance.new("Tool")
 	tool.RequiresHandle = false
-	tool.Name = "Equip to Click TP"
+	tool.Name = "Teleport Tool"
 	tool.Activated:connect(function()
 		local pos = mouse.Hit+Vector3.new(0,2.5,0)
 		pos = CFrame.new(pos.X,pos.Y,pos.Z)
@@ -119,21 +132,171 @@ ToolsSection:NewButton("TpTool", "Teleport Tool", function()
 	tool.Parent = game.Players.LocalPlayer.Backpack
 end)
 ToolsSection:NewButton("Btools", "Btools [Visual]", function()
-	for i = 1, 4 do
-		local Tool = Instance.new("HopperBin")
-		Tool.BinType = i
-		Tool.Name = randomString()
-		Tool.Parent = speaker:FindFirstChildOfClass("Backpack")
-	end
+	backpack = game:GetService("Players").LocalPlayer.Backpack
+
+	hammer = Instance.new("HopperBin")
+	hammer.Name = "Hammer"
+	hammer.BinType = 4
+	hammer.Parent = backpack
+
+	cloneTool = Instance.new("HopperBin")
+	cloneTool.Name = "Clone"
+	cloneTool.BinType = 3
+	cloneTool.Parent = backpack
+
+	grabTool = Instance.new("HopperBin")
+	grabTool.Name = "Grab"
+	grabTool.BinType = 2
+	grabTool.Parent = backpack
 end)
 -- More Tab
 MoreSection:NewButton("ReJoin", "Rejoin On the Server", function()
 	TeleportService:Teleport(game.PlaceId, LocalPlayer)
-    print("Clicked")
 end)
 -- KeyBind GUI
 MoreSection:NewKeybind("Toggle Gui", "Show / Hide Gui", Enum.KeyCode.LeftAlt, function()
 	Library:ToggleUI()
+end)
+MoreSection:NewButton("Radio Gui", "Play your own sounds [Visual]", function()
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local Play = Instance.new("TextButton")
+local SoundTrack = Instance.new("Sound")
+local UICorner_2 = Instance.new("UICorner")
+local Stop = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local TextBox = Instance.new("TextBox")
+local Exit = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+
+local gui = Frame
+
+local dragging
+local dragInput
+local dragStart
+local startPos
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+SoundTrack.Parent = ScreenGui
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(105, 106, 96)
+Frame.BackgroundTransparency = 0.250
+Frame.Position = UDim2.new(0.0436991863, 0, 0.592730999, 0)
+Frame.Size = UDim2.new(0, 182, 0, 97)
+
+UICorner.CornerRadius = UDim.new(0, 15)
+UICorner.Parent = Frame
+
+Play.Name = "Play"
+Play.Parent = Frame
+Play.BackgroundColor3 = Color3.fromRGB(0, 255, 21)
+Play.Position = UDim2.new(0.0494505502, 0, 0.556701005, 0)
+Play.Size = UDim2.new(0, 81, 0, 31)
+Play.Font = Enum.Font.SourceSans
+Play.Text = "Play"
+Play.TextColor3 = Color3.fromRGB(0, 0, 0)
+Play.TextScaled = true
+Play.TextSize = 14.000
+Play.TextWrapped = true
+
+UICorner_2.CornerRadius = UDim.new(0, 15)
+UICorner_2.Parent = Play
+
+Stop.Name = "Stop"
+Stop.Parent = Frame
+Stop.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Stop.Position = UDim2.new(0.494505495, 0, 0.556701005, 0)
+Stop.Size = UDim2.new(0, 81, 0, 31)
+Stop.Font = Enum.Font.SourceSans
+Stop.Text = "Stop"
+Stop.TextColor3 = Color3.fromRGB(0, 0, 0)
+Stop.TextScaled = true
+Stop.TextSize = 14.000
+Stop.TextWrapped = true
+
+UICorner_3.CornerRadius = UDim.new(0, 15)
+UICorner_3.Parent = Stop
+
+TextBox.Parent = Frame
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.BackgroundTransparency = 0.300
+TextBox.Position = UDim2.new(0.0769230798, 0, 0.0824742243, 0)
+TextBox.Size = UDim2.new(0, 153, 0, 35)
+TextBox.Font = Enum.Font.SourceSans
+TextBox.ClearTextOnFocus = false
+TextBox.PlaceholderColor3 = Color3.fromRGB(40, 40, 40)
+TextBox.PlaceholderText = "Id pls..."
+TextBox.Text = ""
+TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.TextSize = 22.000
+
+Exit.Name = "Exit"
+Exit.Parent = Frame
+Exit.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Exit.Position = UDim2.new(0.917582393, 0, -0.144329891, 0)
+Exit.Size = UDim2.new(0, 28, 0, 30)
+Exit.Font = Enum.Font.SourceSans
+Exit.Text = "X"
+Exit.TextColor3 = Color3.fromRGB(0, 0, 0)
+Exit.TextScaled = true
+Exit.TextSize = 14.000
+Exit.TextWrapped = true
+
+UICorner_4.CornerRadius = UDim.new(0, 15)
+UICorner_4.Parent = Exit
+Play.MouseButton1Click:Connect(function()
+	if TextBox.Text ~= "" then
+		SoundTrack.SoundId = "rbxassetid://"..TextBox.Text
+		SoundTrack:Play()
+	end
+end)
+
+Stop.MouseButton1Click:Connect(function()
+	SoundTrack:Stop()
+end)
+
+Exit.MouseButton1Click:Connect(function()
+	ScreenGui:Destroy()
+end)
+
+
+
+local function update(input)
+	local delta = input.Position - dragStart
+	gui.Position = gui:TweenPosition(UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y), 'Out', 'Linear', 0, true); -- drag speed
+end
+
+gui.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+		dragging = true
+		dragStart = input.Position
+		startPos = gui.Position
+
+		input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+			end
+		end)
+	end
+end)
+
+gui.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+		dragInput = input
+	end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+	if input == dragInput and dragging then
+		update(input)
+	end
+end)
+print("Loaded Gui")
 end)
 -- Color Picker
 local PickerTheme = More:NewSection("Custom Theme")
