c02730d601d8cb7a7f5743289906089dac68924d
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-03-02 11:28:58 +0500

    Update LoaderHub.lua

--- LoaderHub.lua
+++ LoaderHub.lua
@@ -1,17 +1,18 @@
 local games = {
     [{9848789324}] = "https://raw.githubusercontent.com/i4mitty/InfernoHub/main/InfernoHub.lua",
-    [{142823291}] = "getgenv().mainKey = "nil"
-
-local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()",
-    [{621129760}] = "",
+    [{142823291}] = "https://raw.githubusercontent.com/vova999000333/Hubs/main/EclipseHubLink.lua",
+    [{621129760}] = "https://raw.githubusercontent.com/vova999000333/Hubs/main/KAT_aim.lua",
     [{155615604}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Prison%20Life/Script.lua",
-    [{7041939546}] = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",
+    [{7041939546}] = "https://raw.githubusercontent.com/vova999000333/Hubs/main/OriganeHub.lua",
     [{6839171747}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Loader.lua",
 }
 
 for ids, url in next, games do
     if table.find(ids, game.PlaceId) then
         loadstring(game:HttpGet(url))()
+        break
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vova999000333/Hubs/main/OriganeHub.lua"))()
         break
     end
 end
