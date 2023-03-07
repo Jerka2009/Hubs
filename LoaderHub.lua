1f4165a5587b765e094c2ad55ce5f075a54a6362
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-03-07 16:56:40 +0500

    Update LoaderHub.lua

--- LoaderHub.lua
+++ LoaderHub.lua
@@ -1,6 +1,7 @@
+local selectid = false
 local games = {
     [{9848789324}] = "https://raw.githubusercontent.com/i4mitty/InfernoHub/main/InfernoHub.lua",
-    [{142823291}] = "https://raw.githubusercontent.com/vova999000333/Hubs/main/EclipseHubLink.lua",
+    [{142823291}] = "https://raw.githubusercontent.com/vova999000333/Hubs/main/OriganeHub.lua",
     [{621129760}] = "https://raw.githubusercontent.com/vova999000333/Hubs/main/KAT_aim.lua",
     [{155615604}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Prison%20Life/Script.lua",
     [{7041939546}] = "https://raw.githubusercontent.com/vova999000333/Hubs/main/OriganeHub.lua",
@@ -10,6 +11,10 @@ local games = {
 for ids, url in next, games do
     if table.find(ids, game.PlaceId) then
         loadstring(game:HttpGet(url))()
+        selectid = true
         break
     end
+end
+if not selectid then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vova999000333/Hubs/main/OriganeHub.lua"))()
 end
