00427c5da2eb510a6c4048083b67e6f31cb55397
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-04-29 18:27:33 +0500

    Update KATLoader.lua

--- KATLoader.lua
+++ KATLoader.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Origane - Kat", "Midnight")
+local Window = Library.CreateLib("Organic - Kat", "Midnight")
 local Player = Window:NewTab("Player")
 local KAT = Window:NewTab("KAT")
 local Tools = Window:NewTab("Tools")
