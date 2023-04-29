170a94339b7c3b0811dd4bb0cefea14d67e42814
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-04-29 18:14:22 +0500

    Update OriganeJailbreak.lua

--- OriganeJailbreak.lua
+++ OriganeJailbreak.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Origane - Jailbreak", "Synapse")
+local Window = Library.CreateLib("Organic - Jailbreak", "Synapse")
 local Player = Window:NewTab("Player")
 local JBTab = Window:NewTab("Jailbreak")
 local ESPtab = Window:NewTab("ESP")
