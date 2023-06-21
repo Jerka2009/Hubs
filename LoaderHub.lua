4e5e6ef5b50b96d732d8aa6f0d7492acd47c724c
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-06-21 13:04:14 +0500

    Update LoaderHub.lua

--- LoaderHub.lua
+++ LoaderHub.lua
@@ -14,6 +14,7 @@
 ]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vova999000333/Hubs/main/intro.lua"))()
 local selectid = false
+local plr = game:GetService("Players").LocalPlayer
 local games = {
     [{9848789324}] = "https://raw.githubusercontent.com/i4mitty/MysteryHub/main/MysteryHub%20-%20Ragdoll%20Engine.lua",
     [{606849621}] = "https://raw.githubusercontent.com/vova999000333/Hubs/main/OriganeJailbreak.lua",
@@ -24,7 +25,7 @@ local games = {
     [{7041939546}] = "https://raw.githubusercontent.com/vova999000333/Hubs/main/OriganeHub.lua",
     [{6839171747}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Loader.lua",
 }
-if plr:FindFirstChild("HasKey") then
+if plr:FindFirstChild("HasKey") ~= nil then
   if plr:FindFirstChild("HasKey").Value == true then
     for ids, url in next, games do
     if table.find(ids, game.PlaceId) then
