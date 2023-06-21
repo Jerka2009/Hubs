bdcbbccfa5e577ee84433867798263e26df9c7e8
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-06-21 12:36:29 +0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -9,7 +9,7 @@ OrionLib:MakeNotification({
 	Time = 5
 })
 
-_G.Key = loadstring(game:HttpGet("https://raw.githubusercontent.com/vova999000333/Hubs/main/key.lua"))()
+_G.Key = loadstring(game:HttpGet("https://raw.githubusercontent.com/vova999000333/Hubs/main/Key.lua"))()
 _G.KeyInput = "string"
 _G.HasKey = false
 
