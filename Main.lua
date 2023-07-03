85498f6cb6d5222781a249c9a897fc8f63d1ffa5
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-07-03 11:21:06 +0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -17,27 +17,59 @@ function MakeScriptHub()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/vova999000333/Hubs/main/LoaderHub.lua"))()
 end
 
+function Login()
+	game.StarterGui:SetCore("SendNotification", {
+    Title = "Log in";
+    Text = "You're logged as "..plr.Name..". \n Please, enter your key.";
+    Icon = "4625478093";
+    Duration = "4";
+})
+end
+
+function INcorrect()
+	game.StarterGui:SetCore("SendNotification", {
+    Title = "Incorrect key!";
+    Text = "pls get key, for login";
+    Icon = "11745872952";
+    Duration = "3";
+})
+end
+function COrrect()
+	game.StarterGui:SetCore("SendNotification", {
+    Title = "Correct key!";
+    Text = "Enjoy the script!";
+    Icon = "12690727261";
+    Duration = "2";
+})
+end
+
 local Tab = Window:NewTab("Key")
 local Sect = Tab:NewSection("Key system")
 
-Tab:NewTextBox("Key", "kekekkek", function(txt)
+Sect:NewTextBox("Key", "kekekkek", function(txt)
 	_G.KeyInput = txt
 end)
 Sect:NewButton("Check key", "button", function()
     if _G.KeyInput == _G.Key then
-	wait(1)
-	plr:FindFirstChild("HasKey").Value = true
-	if plr:FindFirstChild("HasKey").Value == true then
-		for i,v in pairs(game.CoreGui:GetChildren()) do
-        if v:IsA("ScreenGui") and v.Name == kavName then
-            v:Destroy()
-		MakeScriptHub()
-        end
-    end
+		wait(1)
+		plr:FindFirstChild("HasKey").Value = true
+		if plr:FindFirstChild("HasKey").Value == true then
+			COrrect()
+			wait(1)
+			for i,v in pairs(game.CoreGui:GetChildren()) do
+				if v:IsA("ScreenGui") and v.Name == kavName then
+					v:Destroy()
+				end
+			end
+			wait()
+			MakeScriptHub()
 		end
 	end
+  else
+		INcorrect()
   end    
 end)
 Sect:NewButton("Get key", "button", function()
 	setclipboard("https://discord.gg/4Kp6QkfNev")
 end)
+Login()
