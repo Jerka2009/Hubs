a7cc24f8fecd343cfe82662713ee379e9a3384f4
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-07-12 09:51:37 +0500

    Update MurderVSsheriff.lua

--- MurderVSsheriff.lua
+++ MurderVSsheriff.lua
@@ -417,7 +417,7 @@ end)
 game:GetService('RunService').RenderStepped:connect(function()
 if _G.Disabled then
 	for i,v in next, game:GetService('Players'):GetPlayers() do
-		if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+		if v.Name ~= game:GetService('Players').LocalPlayer.Name and v.Character.Highlight == nil then
 			pcall(function()
 				v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
 				v.Character.HumanoidRootPart.Transparency = 0.7 -- original: 0.7
