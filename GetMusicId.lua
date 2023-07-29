87beb7e03ef46b0db26f51acaa3a4042ae47b6f3
Jerka2009 <90212510+vova999000333@users.noreply.github.com>
2023-07-29 19:35:55 +0500

    Create GetMusicId.lua

---
+++ GetMusicId.lua
@@ -0,0 +1,58 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Organic - Get Sound Id!", "Synapse")
+local Control = Window:NewTab("Control")
+local More = Window:NewTab("More")
+local ControlSec = Control:NewSection("--Get Sound--")
+local MoreSection = More:NewSection("More")
+-- Variables
+local ToolName = "BoomBox"
+local PlayerSelected = ""
+local plrsTable = {}
+-- Control Tab
+for i, plr in pairs(game:GetService("Players"):GetPlayers()) do
+	table.insert(plrsTable, plr.Name)
+end
+local Label = ControlSec:NewLabel("{SoundID}")
+local plrs = ControlSec:NewDropdown("Player", "PlayerList", plrsTable, function(currentOption)
+    PlayerSelected = currentOption
+end)
+game:GetService("Players").PlayerRemoving:Connect(function(plr)
+	local index = table.find(plrsTable, plr.Name) --get the index
+	table.remove(plrsTable, index) --remove the index
+	plrs:Refresh(plrsTable)
+end)
+ControlSec:NewButton("GetSound from player", "Get Sound ID", function()
+	if game:GetService("Players"):FindFirstChild(PlayerSelected) then
+		local player = game:GetService("Players"):FindFirstChild(PlayerSelected)
+		if player.Character then
+			local char = player.Character
+			if char:FindFirstChild(ToolName) ~= nil then
+			local Box = char:FindFirstChild(ToolName)
+				local Sound = Box:WaitForChild("Handle"):WaitForChild("Sound")
+				local id = Sound.SoundId
+				local h = tostring(id)
+				local PrintText = string.sub(h,14,30)
+				Label:UpdateLabel(PrintText)
+        setclipboard(PrintText)
+			else
+				game.StarterGui:SetCore("SendNotification", {
+    				Title = "Error!";
+    				Text = "Player "..PlayerSelected.." is not eqiup the "..ToolName.."!";
+    				Icon = "4625478093";
+    				Duration = 2;
+				})
+			end
+		end
+	else
+		game.StarterGui:SetCore("SendNotification", {
+    	Title = "Error!";
+    	Text = "Player "..PlayerSelected.." is not found in Players!";
+    	Icon = "4625478093";
+    	Duration = 2;
+    })
+	end
+end)
+--More Tab
+MoreSection:NewButton("ReJoin", "Rejoin On the Server", function()
+	TeleportService:Teleport(game.PlaceId, LocalPlayer)
+end)
