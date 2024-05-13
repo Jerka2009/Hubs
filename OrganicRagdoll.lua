bb314dfb1dd01c225593b4fc48b9e7a8ec2e052c
Jerka2009 <90212510+Jerka2009@users.noreply.github.com>
2024-05-13 21:20:17 +0500

    Update OrganicRagdoll.lua

--- OrganicRagdoll.lua
+++ OrganicRagdoll.lua
@@ -1,2 +1,2 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/vova999000333/Hubs/main/UI-LibraryOrganic.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jerka2009/Hubs/main/UI-LibraryOrganic.lua"))()
 local Window = Library.CreateLib("Organic - Ragdoll Engine", "LightTheme")
